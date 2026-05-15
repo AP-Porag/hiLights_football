c78e4be1958ad1dd7a96086394f8da76434f8ba3
AP-Porag <smauhp@gmail.com>
2026-05-16 04:00:10 +0600

    Add structured routing for players, scouting, and admin sections

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+//all web routes
 Route::get('/', function () {
     return Inertia::render('web/Home');
 })->name('home');
@@ -19,16 +20,61 @@ Route::get('/contact', function () {
     return Inertia::render('web/Contact');
 })->name('contact');
 
+//all player routes
 Route::prefix('player')->group(function () {
+
     Route::get('/', function () {
         return Inertia::render('player/dashboard/Index');
-    })->name('player-dashboard');
+    })->name('dashboard');
 });
 
-Route::middleware(['auth'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
+//all Scouts / Agents / Clubs routes
+Route::prefix('scouting')->group(function () {
+
+    Route::get('/', function () {
+        return Inertia::render('scouting/dashboard/Index');
     })->name('dashboard');
+
+    Route::get('/search', function () {
+            return Inertia::render('scouting/search/Index');
+        })->name('search.player');
+
+    Route::get('/player-profile', function () {
+                return Inertia::render('scouting/player/details');
+            })->name('player.details');
+});
+
+
+//all admin routes
+Route::middleware(['auth'])->prefix('admin')->group(function () {
+
+    Route::get('/', function () {
+        return Inertia::render('admin/dashboard/Index');
+    })->name('dashboard');
+
+    Route::get('/users', function () {
+        return Inertia::render('admin/users/Index');
+    })->name('users.index');
+
+    Route::get('/players', function () {
+        return Inertia::render('admin/players/Index');
+    })->name('players.index');
+
+    Route::get('/players/{id}', function () {
+        return Inertia::render('admin/players/details');
+    })->name('players.details');
+
+    Route::get('/players/{id}', function () {
+        return Inertia::render('admin/players/details');
+    })->name('players.details');
+
+    Route::get('/subscriptions', function () {
+        return Inertia::render('admin/subscriptions/Index');
+    })->name('subscriptions.index');
+
+    Route::get('/scouting', function () {
+        return Inertia::render('admin/scouting/Index');
+    })->name('scouting.index');
 });
 
 require __DIR__.'/settings.php';
