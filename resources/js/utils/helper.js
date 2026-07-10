ac92bc854839cf6da87c97bacb8b5a4f7b348085
unknown <razaulk127@gmail.com>
2026-07-10 18:18:49 +0600

    updated

--- resources/js/utils/helper.js
+++ resources/js/utils/helper.js
@@ -1,11 +1,12 @@
 export const getPositionName = (positions = []) => {
     const positionMap = {
         LB: 'LEFT BACK',
+        LM: 'LEFT MIDFIELDER',
         RB: 'RIGHT BACK',
         CB: 'CENTER BACK',
         CM: 'CENTRAL MIDFIELDER',
         'CM-L': 'LEFT CENTRAL MIDFIELDER',
-        'CM-R': 'RIGHT CENTRAL MIDFIELDER',
+        'CB-R': 'RIGHT CENTRAL MIDFIELDER',
         CAM: 'ATTACKING MIDFIELDER',
         CDM: 'DEFENSIVE MIDFIELDER',
         LW: 'LEFT WINGER',
